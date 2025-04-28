d149aa0827dc7420d04ae44b5d5680c74f967866
frknsrky <86148025+frknsrky@users.noreply.github.com>
2025-04-28 15:40:08 -0400

    Update perf_api.c

--- hotelReservation/services/perf/perf_api.c
+++ hotelReservation/services/perf/perf_api.c
@@ -42,7 +42,7 @@ int perf_start() {
         pe.exclude_kernel = 1;
         pe.exclude_hv = 1;
 
-        leader_fd = perf_event_open(&pe, 0, -1, -1, 0);
+        leader_fd = perf_event_open(&pe, 0, 0, -1, 0);
         if (leader_fd == -1) {
             perror("perf_event_open (cycles)");
             return -1;
@@ -51,7 +51,7 @@ int perf_start() {
         // Instructions
         pe.disabled = 0;
         pe.config = PERF_COUNT_HW_INSTRUCTIONS;
-        instructions_fd = perf_event_open(&pe, 0, -1, leader_fd, 0);
+        instructions_fd = perf_event_open(&pe, 0, 0, leader_fd, 0);
         if (instructions_fd == -1) {
             perror("perf_event_open (instructions)");
             close(leader_fd);
@@ -63,7 +63,7 @@ int perf_start() {
         pe.config = PERF_COUNT_HW_CACHE_L1D |
                     (PERF_COUNT_HW_CACHE_OP_READ << 8) |
                     (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
-        l1_misses_fd = perf_event_open(&pe, 0, -1, leader_fd, 0);
+        l1_misses_fd = perf_event_open(&pe, 0, 0, leader_fd, 0);
         if (l1_misses_fd == -1) {
             perror("perf_event_open (l1_misses)");
             close(leader_fd);
