587951bea6efb2846b4b59c30ba45f71949a7d4a
frknsrky <86148025+frknsrky@users.noreply.github.com>
2025-05-01 14:16:15 -0400

    Update perf_api.h

--- hotelReservation/services/perf/perf_api.h
+++ hotelReservation/services/perf/perf_api.h
@@ -1,10 +1,16 @@
 #ifndef PERF_API_H
 #define PERF_API_H
 
+struct perf_handles {
+    int leader_fd;
+    int instructions_fd;
+    int l1_misses_fd;
+};
+
 // Start performance counters
-int perf_start();
+perf_handles perf_start();
 
 // Stop counters and return a result string like: "cycles=..., instructions=..., l1_misses=..."
-const char* perf_stop();
+const char* perf_stop(int leader_fd, int instructions_fd, int l1_misses_fd);
 
 #endif // PERF_API_H
